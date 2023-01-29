620ec78342262fb833eb671d461cc1a3dd746d5d
ttm68dark <ttm68dark@gmail.com>
2023-01-29 16:09:10 +0700

    update project

--- components/ProjectItem/ProjectItem.jsx
+++ components/ProjectItem/ProjectItem.jsx
@@ -30,62 +30,127 @@ function ProjectItem({}) {
         </div>
       </div>
 
-      {/* Reactjs Travel web  */}
-      <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
-        <div className="text-black dark:text-white">
-          <Link href="https://travel-web-3e599.firebaseapp.com/" passHref>
-            <a target="_blank">
-              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
-                Blog Du Lịch
-              </h4>
-              <img src="/dulich.PNG" alt="newstore" />
-            </a>
-          </Link>
-
-          <h5 className="font-bold my-2">
-            Project Reactjs + RealTime Database Firebase ( google )
-          </h5>
-          <p>
-            - Xây dựng demo một Blog website nhỏ chia sẻ các bài viết về du
-            lịch, những bức ảnh checkin thú vị.
-          </p>
-          <p>
-            - Xây dựng chức năng đăng ký, đăng nhập, phân quyền, admin có thể
-            đăng mới, cập nhật, chỉnh sửa bài viết,...
-          </p>
-          <p>
-            - Dùng React Hook Form để thực hiện Validation cho form đăng kí,
-            đăng nhập, form thêm bài viết, kiểm tra ảnh được upload,...
-          </p>
-          <p>- ✨ Giao diện làm với Material Ui</p>
-        </div>
-      </div>
-
       {/* Bán hàng  */}
       <div className="col-span-12 md:col-span-6 border p-2 font-sans text-xs lg:text-sm">
         <div className="text-black dark:text-white">
           <Link href="https://webapp-six-omega.vercel.app/" passHref>
             <a target="_blank">
               <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
-                WebSite bán Hàng
+                Tổ hợp Web marketplace,launchpad web,...
               </h4>
-              <img src="/store.PNG" alt="newstore" />
+              <img src="/pyra1.png" alt="newstore" />
             </a>
           </Link>
-          <h3 className="text-red-600">Hiện đang được cập nhật</h3>
-          <h5 className="text-red-600">Đang cập nhật lại...</h5>
-          <h5 className="font-bold my-2">
-            WebSite bán Quần áo sử dụng Nextjs FrameWork + Material Ui
-          </h5>
+
+          <h5 className="font-bold my-2">Tổ hợp 3 website</h5>
+
+          <Link href="https://marketplace.pyramidwalk.com/" passHref>
+            <a target="_blank">
+              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
+                Link web marketplace
+              </h5>
+            </a>
+          </Link>
+          <Link href="https://webapp-six-omega.vercel.app/" passHref>
+            <a target="_blank">
+              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
+                Link Pyramid web
+              </h5>
+            </a>
+          </Link>
+          <Link href="https://webapp-six-omega.vercel.app/" passHref>
+            <a target="_blank">
+              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
+                Link Launchpad web
+              </h5>
+            </a>
+          </Link>
+
+          <p>- Để trải nghiệm web bạn cần có ví metamask để đăng nhập</p>
 
           <p>
-            - Sử dụng Nextjs để tối ưu hóa SEO cho khách hàng, kèm theo một số
-            chức năng để đăng sản phẩm, cập nhật, khuyến mãi,..
+            - 3 website được xây dựng với reactjs, tailwindcss và nhiều thư viện
+            khác,...
           </p>
-
-          <p>- ✨ Giao diện làm với Material Ui ➡ TailWindCss</p>
+          <p>
+            - Database sử dụng cho marketplace, launchpad là moralis và 1 số lưu
+            trữ trên blockchain
+          </p>
         </div>
       </div>
+      {/* Reactjs Travel web  */}
+      <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
+        <div className="text-black dark:text-white">
+          <Link href="https://mklabs.finance/" passHref>
+            <a target="_blank">
+              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
+                MKLABS
+              </h4>
+              <img src="/mklab.PNG" alt="newstore" />
+            </a>
+          </Link>
+
+          <h5 className="font-bold my-2">MKLABS</h5>
+          <p>- Để trải nghiệm web bạn cần có ví metamask để đăng nhập</p>
+          <p>- ✨ Giao diện làm với scss</p>
+        </div>
+      </div>
+      <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
+        <div className="text-black dark:text-white">
+          <Link href="https://247cryp.to/" passHref>
+            <a target="_blank">
+              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
+                247 Crypto
+              </h4>
+              <img src="/cryp.PNG" alt="newstore" />
+            </a>
+          </Link>
+
+          <h5 className="font-bold my-2">247CRYP</h5>
+
+          <p>- ✨ Giao diện làm với Scss/ và 1 số thư viện khác</p>
+        </div>
+      </div>
+
+      {/* tiktok blog  */}
+      <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
+        <div className="text-black dark:text-white">
+          <Link href="https://tiktok-blue.vercel.app/" passHref>
+            <a target="_blank">
+              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
+                TIKTOK BLOG
+              </h4>
+              <img src="/tiktok.PNG" alt="newstore" />
+            </a>
+          </Link>
+
+          <h5 className="font-bold my-2">TIKTOK BLOG</h5>
+          <p>- Tự lên ý tưởng thiết kế giao diện bằng figma, và hiện thực</p>
+          <p>
+            - Hiện tại chưa phát triển admin manager cho web cũng như database
+          </p>
+
+          <p>- ✨ Giao diện làm với tailwindcss</p>
+        </div>
+      </div>
+
+      <div className="col-span-12 md:col-span-6 border p-2 font-sans text-xs lg:text-sm">
+        <div className="text-black dark:text-white">
+          <Link href="https://bundaurengreng.vn/" passHref>
+            <a target="_blank">
+              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
+                Bún Đậu RengReng
+              </h4>
+              <img src="/bun.PNG" alt="newstore" />
+            </a>
+          </Link>
+
+          <h5 className="font-bold my-2">
+            Trang web giới thiệu cho 1 cửa hàng bún đậu mắm tôm
+          </h5>
+        </div>
+      </div>
+
       {/* Unimart unitop  */}
       <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
         <div className="text-black dark:text-white">
@@ -99,7 +164,6 @@ function ProjectItem({}) {
           </Link>
 
           <h5 className="font-bold my-2">Project Laravel + MySQL</h5>
-          <p>- Xây dựng demo một website nhỏ bán điện thoại di động.</p>
           <p>
             - Xây dựng chức năng đăng ký, đăng nhập, phân quyền,tự động gửi mail
             xác nhận đặt hàng, admin có thể theo dõi đơn hàng, cập nhật tình
@@ -133,31 +197,6 @@ function ProjectItem({}) {
           </p>
 
           <p>- ✨ Giao diện làm với Html/Css</p>
-        </div>
-      </div>
-
-      {/* giao diện tạp chí  */}
-      <div className="col-span-12 md:col-span-6 border p-2 font-sans text-xs lg:text-sm">
-        <div className="text-black dark:text-white">
-          <Link
-            href="https://vuongquocbao98.github.io/finnal-template/"
-            passHref
-          >
-            <a target="_blank">
-              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
-                Cắt giao diện từ PSD sang Html bằng Bootstrap
-              </h4>
-              <img src="/giay.PNG" alt="newstore" />
-            </a>
-          </Link>
-
-          <h5 className="font-bold my-2">Cắt giao diện Html bằng Bootstrap</h5>
-          <p>
-            - Cắt giao diện từ file PSD có sẵn sang Html sử dụng Bootstrap theo
-            đúng kích thước, font chữ theo file đã cho.
-          </p>
-
-          <p>- ✨ Giao diện làm với Bootstrap</p>
         </div>
       </div>
     </>
