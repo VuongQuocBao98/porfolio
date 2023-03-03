bce195ef99e5e77489e70cd96af8456160fff313
Vuong Quoc Bao <ttm68dark@gmail.com>
2023-03-03 11:24:58 +0700

    update ui

--- components/ProjectItem/ProjectItem.jsx
+++ components/ProjectItem/ProjectItem.jsx
@@ -17,6 +17,8 @@ function ProjectItem({}) {
           </Link>
 
           <h5 className="font-bold my-2">Project Reactjs + Strapi api</h5>
+          <p>- Team Size: 1</p>
+          <p>- Timeline: 3 day</p>
           <p>
             - Build a demo version for a small website with product list,
             detail, cart and register/login pages.
@@ -26,38 +28,40 @@ function ProjectItem({}) {
             - Use React Hook Form and Yup for form validation in sign up and
             login.
           </p>
+
           <p>- ✨ Interface made with Material UI</p>
         </div>
       </div>
 
-      {/* Bán hàng  */}
+      {/* soccer world  */}
       <div className="col-span-12 md:col-span-6 border p-2 font-sans text-xs lg:text-sm">
         <div className="text-black dark:text-white">
-          <Link href="https://pyramidpad.io/" passHref>
+          <Link href="https://game.soccerworld.finance/" passHref>
             <a target="_blank">
               <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
-                Web marketplace, web launchpad, etc.
+                SoccerWorld Blockchain GameFi
               </h4>
-              <img src="/py.png" alt="newstore" />
+              <div className="flex overflow-x-scroll">
+                <img src="sc1.png" className="w-[90%]" alt="newstore" />
+                <img src="scc.png" className="flex-1" alt="newstore" />
+                <img src="sc.png" className="flex-1" alt="newstore" />
+              </div>
             </a>
           </Link>
+          <h5 className="font-bold my-2">SoccerWorld Blockchain GameFi</h5>
+          <p>- Team size: 3.</p>
+          <p>- Responsiable: </p>
+          <p> + Team lead: Control task list/timeline/team report</p>
+          <p> + Support team members if they have problems</p>
+          <p> + Build Moralis Database for game systems and smart contracts</p>
+          <p> + Build feature game island/login</p>
+          <p> + Interact/Test smart contract before team use</p>
+          <p>- To experience the web, you need a MetaMask wallet to log in.</p>
+          <p>
+            - Websites built with Reactjs, scss/css, and various other libraries
+          </p>
+          <p>- The databases Moralis and some storage on the blockchain</p>
 
-          <h5 className="font-bold my-2">3 website</h5>
-
-          <Link href="https://marketplace.pyramidwalk.com/" passHref>
-            <a target="_blank">
-              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
-                Link web marketplace
-              </h5>
-            </a>
-          </Link>
-          <Link href="https://pyramidpad.io/" passHref>
-            <a target="_blank">
-              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
-                Link Pyramid web
-              </h5>
-            </a>
-          </Link>
           <Link href="https://launchpad.pyramidpad.io/" passHref>
             <a target="_blank">
               <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
@@ -65,36 +69,35 @@ function ProjectItem({}) {
               </h5>
             </a>
           </Link>
-
-          <p>- To experience the web, you need a MetaMask wallet to log in.</p>
-
+          <p>- Team size: 2.</p>
+          <p>- Responsiable: </p>
           <p>
-            - 3 websites built with Reactjs, Tailwindcss, and various other
-            libraries
-          </p>
-          <p>
-            - The databases used for the marketplace and launchpad are Moralis
-            and some storage on the blockchain
+            {" "}
+            + Join to update Ui, control and test modular interact contract
           </p>
         </div>
       </div>
-      {/* Reactjs Travel web  */}
+
+      {/* soccer home  */}
       <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
         <div className="text-black dark:text-white">
-          <Link href="https://mklabs.finance/" passHref>
+          <Link href="https://247cryp.to/" passHref>
             <a target="_blank">
               <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
-                MKLABS
+                SoccerWorld
               </h4>
-              <img src="/mklab.PNG" alt="newstore" />
+              <img src="/dd.PNG" alt="newstore" />
             </a>
           </Link>
 
-          <h5 className="font-bold my-2">MKLABS</h5>
-          <p>- To experience the web, you need a MetaMask wallet to log in</p>
-          <p>- ✨ Interface made with scss</p>
+          <h5 className="font-bold my-2">SoccerWorld</h5>
+          <p>- Team size: 1.</p>
+          <p>- Responsiable: </p>
+          <p> + Build Client with Reactjs</p>
+          <p>- ✨ Interface made with taiwindcss and other libraries</p>
         </div>
       </div>
+
       <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
         <div className="text-black dark:text-white">
           <Link href="https://247cryp.to/" passHref>
@@ -107,7 +110,10 @@ function ProjectItem({}) {
           </Link>
 
           <h5 className="font-bold my-2">247CRYP</h5>
-
+          <p>- Team size: 1.</p>
+          <p>- Timeline: 1 day</p>
+          <p>- Responsiable: </p>
+          <p> + Build Client with Reactjs</p>
           <p>- ✨ Interface made with Scss/ and other libraries</p>
         </div>
       </div>
@@ -148,10 +154,91 @@ function ProjectItem({}) {
               <img src="/bun.PNG" alt="newstore" />
             </a>
           </Link>
-
           <h5 className="font-bold my-2">
             Introduction website for a store bún đậu mắm tôm
           </h5>
+          <p>- Team size: 1 (freeland)</p>
+          <p>- Timeline: about 3.5 day</p>
+          <p>- Website built using Reactjs, taiwindcss</p>
+        </div>
+      </div>
+
+      {/* Pyramid  */}
+      <div className="col-span-12 md:col-span-6 border p-2 font-sans text-xs lg:text-sm">
+        <div className="text-black dark:text-white">
+          <Link href="https://pyramidpad.io/" passHref>
+            <a target="_blank">
+              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
+                Web marketplace, web launchpad, etc.
+              </h4>
+              <img src="/py.png" alt="newstore" />
+            </a>
+          </Link>
+          <h5 className="font-bold my-2">3 website</h5>
+          <p>- To experience the web, you need a MetaMask wallet to log in.</p>
+          <p>
+            - 3 websites built with Reactjs, Tailwindcss, and various other
+            libraries
+          </p>
+          <p>
+            - The databases used for the marketplace and launchpad are Moralis
+            and some storage on the blockchain
+          </p>
+          <Link href="https://marketplace.pyramidwalk.com/" passHref>
+            <a target="_blank">
+              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
+                Link web marketplace
+              </h5>
+            </a>
+          </Link>
+          <Link href="https://pyramidpad.io/" passHref>
+            <a target="_blank">
+              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
+                Link Pyramid web
+              </h5>
+            </a>
+          </Link>
+          <p>- Team size: 1.</p>
+          <p>- Responsiable: </p>
+          <p> + Build Moralis Database</p>
+          <p> + Build Client with Reactjs</p>
+          <p> + Interact with smart contract</p>
+
+          <Link href="https://launchpad.pyramidpad.io/" passHref>
+            <a target="_blank">
+              <h5 className="font-medium underline-offset-2 underline my-1 text-blue-500">
+                Link Launchpad web
+              </h5>
+            </a>
+          </Link>
+          <p>- Team size: 2.</p>
+          <p>- Responsiable: </p>
+          <p>
+            {" "}
+            + Join to update Ui, control and test modular interact contract
+          </p>
+        </div>
+      </div>
+      {/* Mklabs  */}
+      <div className="col-span-12 md:col-span-6  border p-2 font-sans text-xs lg:text-sm">
+        <div className="text-black dark:text-white">
+          <Link href="https://mklabs.finance/" passHref>
+            <a target="_blank">
+              <h4 className="text-green-500 font-bold hover:cursor-pointer my-2">
+                MKLABS
+              </h4>
+              <img src="/mklab.PNG" alt="newstore" />
+            </a>
+          </Link>
+          <h5 className="font-bold my-2">MKLABS</h5>
+          <p>- Team size: 3.</p>
+          <p>- Responsiable: </p>
+          <p> + Team lead</p>
+          <p> + Build Moralis Database for feature predition</p>
+          <p> + Build Predition Feature</p>
+          <p> + Support member in team</p>
+          <p>- To experience the web, you need a MetaMask wallet to log in</p>
+          <p>- ✨ Interface made with scss</p>
         </div>
       </div>
 
@@ -168,6 +255,7 @@ function ProjectItem({}) {
           </Link>
 
           <h5 className="font-bold my-2">Project Laravel + MySQL</h5>
+          <p>- Team size: 1 </p>
           <p>
             - Build the function of registration, login, authorization,
             automatic mailing order confirmation, admin can track order, update
